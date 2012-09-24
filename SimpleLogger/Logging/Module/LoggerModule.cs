7cf9973631207bc28952bbd1700312922b23f40c
jirkapenzes <jirkapenzes@hotmail.com>
2012-09-24 12:29:45 +0200

    Removing IDisposable interface.

--- SimpleLogger/Logging/Module/LoggerModule.cs
+++ SimpleLogger/Logging/Module/LoggerModule.cs
@@ -2,11 +2,10 @@ using System;
 
 namespace SimpleLogger.Logging.Module
 {
-    public abstract class LoggerModule : IDisposable
+    public abstract class LoggerModule
     {
         public virtual void BeforeLog() { }
         public virtual void AfterLog(LogMessage logMessage) { }
         public virtual void ExceptionLog(Exception exception) { }
-        public virtual void Dispose() { }
     }
 }
