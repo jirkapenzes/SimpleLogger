a21efaadd2178c65744f4e327c54ddc2478c04d3
Douglas Rocha <douglasbellonrocha@gmail.com>
2016-04-14 13:55:53 -0300

    Added database factory for multiple databases

---
+++ SimpleLogger/Logging/Module/Database/DatabaseFactory.cs
@@ -0,0 +1,39 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLogger.Logging.Module.Database
+{
+    public class DatabaseFactory
+    {
+        public DbConnection GetConnection(DatabaseType databaseType, string connectionString)
+        {
+            switch (databaseType)
+            {
+                case DatabaseType.MsSql:
+                    return new SqlConnection(connectionString);
+                case DatabaseType.Oracle:
+                    return new OracleConnection(connectionString);
+            }
+
+            return null;
+        }
+
+        public DbCommand GetCommand(DatabaseType databaseType, DbConnection connection, string commandText)
+        {
+            switch (databaseType)
+            {
+                case DatabaseType.MsSql:
+                    return new SqlCommand(commandText, connection as SqlConnection);
+                case DatabaseType.Oracle:
+                    return new OracleCommand(commandText, connection as OracleConnection);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SimpleLogger/Logging/Module/Database/DatabaseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLogger.Logging.Module.Database
+{
+    public enum DatabaseType
+    {
+        MsSql = 1,
+        Oracle = 2
+    }
+}
